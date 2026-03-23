115d2e3e68b450e87002577d2140868fb7ccd102
marianjuli <marian.pal@gmail.com>
2026-03-23 17:46:23 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar.jsx"
+
+
+const App = () => {
+    return (
+
+        <main className="App">
+            <NavBar />
+        </main>
+
+
+    )
+}
+export default App
