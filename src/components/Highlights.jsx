2a5cd0fc816eef623cde5bd94055be79a7d671e6
marianjuli <marian.pal@gmail.com>
2026-04-02 15:50:56 -0300

    Final review and corrections

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -9,20 +9,22 @@ const Highlights = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 1024px)" }); //we use the useMediaQuery hook from the react-responsive library to determine if the user is on a mobile device.
   // This allows us to conditionally render or style components based on the screen size, ensuring a responsive design that looks good on both desktop and mobile devices.
 
-  useGSAP(() => {
-    gsap.to([".left-column, .right-column"], {
-      scrollTrigger: {
-        trigger: "#highlights", //we trigger the animation when the highlights section comes into view
-        start: isMobile ? "bottom bottom" : "top top",
-      },
-      y: 0,
-      opacity: 1,
-      stagger: 0.5, //we stagger the  animation of the left and right columns by 0.5 seconds, creating a more dynamic and visually appealing entrance effect as the user scrolls through the highlights section.
-      ease: "power1.out",
-      duration: 1,
-
-    });
-  });
+  useGSAP(
+    () => {
+      gsap.to([".left-column, .right-column"], {
+        scrollTrigger: {
+          trigger: "#highlights", //we trigger the animation when the highlights section comes into view
+          start: isMobile ? "bottom bottom" : "top top",
+        },
+        y: 0,
+        opacity: 1,
+        stagger: 0.5, //we stagger the  animation of the left and right columns by 0.5 seconds, creating a more dynamic and visually appealing entrance effect as the user scrolls through the highlights section.
+        ease: "power1.out",
+        duration: 1,
+      });
+    },
+    { dependencies: [isMobile], revertOnUpdate: true },
+  );
 
   return (
     <section id="highlights">
@@ -44,7 +46,7 @@ const Highlights = () => {
 
         <div className="right-column">
           <div className="apple-gradient">
-            <img src="ai.png" alt="AI" />
+            <img src="/ai.png" alt="AI" />
             <p>
               Built for <br />
               <span>Apple Intelligence</span>
@@ -52,7 +54,7 @@ const Highlights = () => {
           </div>
 
           <div>
-            <img src="battery.png" alt="Battery" />
+            <img src="/battery.png" alt="Battery" />
             <p>
               Up to <br />
               <span className="green-gradient">
